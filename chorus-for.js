a4db87a420640d5dae251a6ae8695019e85318eb
Donnie Walsh <donfather1000@gmail.com>
2021-09-02 02:18:50 +0000

    did the same thing but with a for loop

---
+++ chorus-for.js
@@ -0,0 +1,11 @@
+const chorus = "Let's dance!";
+
+for (let repeat = 0; repeat < 10; repeat ++) {
+  console.log(chorus);
+  if (repeat === 5) {
+    console.log("*key change*");
+  }
+}
+
+console.log("Until the SUN comes UP broooooo");
+
