e81f2137b217f90d2ed669bfb724ce5d7bc5f7d1
Donnie Walsh <donfather1000@gmail.com>
2021-09-02 09:26:57 +0000

    explored functions more and developed and understanding of the difference between returning a value and logging it to the console

---
+++ even.js
@@ -0,0 +1,7 @@
+
+
+//declares function isEven which takes num as a parameter and returns
+// whether or not the number is evenly divisible by 2 (even number)
+const isEven = function (num) {
+  return num % 2 === 0;
+}
--- hello.js
+++ hello.js
@@ -1,7 +1,25 @@
+
+
+//function to concatenate the string hello with the input parameter
 const sayHello = function (name) {
-  console.log("Hello, " +  name );
+  console.log("Hello, " + name);
 }
 
+
+//calling the above function to print names to console
 sayHello("Caliban");
 sayHello("Miranda");
-sayHello("Ferdiand");
+sayHello("Ferdiand"); webkitCancelAnimationFrame
+
+
+//function which is almost identical to the one above, but this time does
+//not print anything intuitively, but returns the concatenated string
+const sayHelloToConsole = function (name) {
+  return "Hello, " + name;
+}
+
+//declaring a greeting to pass to the console.log() after
+const greeting = returnSayHello('John');
+console.log(greeting);
+
+
