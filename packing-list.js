7f0c4c7202833c08ad476137a3755429f01b7a8c
Donnie Walsh <donfather1000@gmail.com>
2021-09-03 05:47:37 +0000

    added another variation using a while loop

--- packing-list.js
+++ packing-list.js
@@ -9,3 +9,9 @@ for (i = 0; i <= packingList.length; i++) {
   console.log(packingList[i]);
 }
 
+let counterVariable = 0;
+while (counterVariable <= packingList.length) {
+  console.log(packingList[counterVariable]);
+  counterVariable++;
+}
+
